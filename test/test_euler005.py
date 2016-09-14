d69756a203dbbc3848f943a1c9da1839f8b93d3f
Anna Holmgren <anna.holmgren@wigetmedia.com>
2016-09-14 14:01:17 +0200

    Euler 005: Smallest multiple

---
+++ test/test_euler005.py
@@ -0,0 +1,23 @@
+__author__ = 'anna'
+
+import unittest
+from collections import Counter
+import euler005
+
+
+class Euler005tests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_find_prime_factors(self):
+        self.assertEquals(Counter(euler005.prime_factors(10)), Counter([2, 5]))
+        self.assertEquals(Counter(euler005.prime_factors(40)), Counter([2, 2, 2, 5]))
+
+    def test_smallest_multiple(self):
+        self.assertEquals(euler005.smallest_multiple(3), 6)
+        self.assertEquals(euler005.smallest_multiple(10), 2520)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
